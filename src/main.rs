bdae0f2fccd05149984fbc01deb4014befc2bc23
Blupegasus0 <amireastmond@gmail.com>
2023-12-01 17:58:37 -0400

    removed loop

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,29 @@ fn decode_calibration (path: String) -> i32 {
     let mut source_file = File::open(path).unwrap();
     let mut line_value = Vec::new();
 
+        source_file.read(&mut buffer).unwrap();
+        for i in 0..BUFFER_SIZE {
+            let char = buffer[i] as char;
+            if char.is_numeric() {
+                line_value.push(char as i32 - DOGSHIT_CONVERSION);
+            } 
+            if char == '\n' {
+                let first = 0; let last = line_value.len() - 1;
+                calibration_value += line_value[first] * 10 + line_value[last];
+                line_value.clear();
+            }
+        }
+
+    calibration_value
+}
+
+/*
+fn decode_calibration_with_buffer (path: String) -> i32 {
+    let mut calibration_value: i32 = 0;
+    let mut buffer = [0u8; BUFFER_SIZE];
+    let mut source_file = File::open(path).unwrap();
+    let mut line_value = Vec::new();
+
     loop {
         let read_count = source_file.read(&mut buffer).unwrap();
         for i in 0..BUFFER_SIZE {
@@ -36,3 +59,4 @@ fn decode_calibration (path: String) -> i32 {
     }
     calibration_value
 }
+*/
